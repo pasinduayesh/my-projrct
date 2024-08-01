48f7994de68c80564ec779a8b22f3db6b32d7cf6
Pasindu ayesh <72848574+pasinduayesh@users.noreply.github.com>
2024-08-01 23:10:39 +0530

    Add files via upload

--- selenium4/src/AutomationScript/Firstscript.java
+++ selenium4/src/AutomationScript/Firstscript.java
@@ -1,37 +1,43 @@
 package AutomationScript;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Firstscript {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dark Lion\\Desktop\\New folder\\chromedriver-win64\\chromedriver.exe");
-		
-		WebDriver driver =new ChromeDriver();
-		
-		driver.get("http://localhost/Online-Gym-Management-System-in-PHP-main/Admin/login.php");
-		
-		//provide username
-				WebElement username = driver.findElement(By.name("email"));
-				username.isDisplayed();
-				username.isEnabled();
-				username.sendKeys("admin@gmail.com");
-				
-				//provide password
-				WebElement password = driver.findElement(By.name("password"));
-				password.isDisplayed();
-				password.isEnabled();
-				password.sendKeys("ayesh12PAK");
-				
-				//clicking on a login button
-				WebElement loginButton = driver.findElement(By.name("submit"));
-				loginButton.isDisplayed();
-				loginButton.isEnabled();
-				loginButton.click();
-		
-	}
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dark Lion\\Desktop\\New folder\\chromedriver-win64\\chromedriver.exe");
 
+        // Instantiate a ChromeDriver class
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=D:\\Chrome\\NewProfile");
+        options.setBinary("C:\\Users\\Dark Lion\\Desktop\\New folder\\chrome-win64\\chrome.exe"); // Set the path to your Chrome binary
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize(); // Maximize Window
+
+        driver.get("http://localhost/Online-Gym-Management-System-in-PHP-main/Admin/login.php");
+
+        // Provide username
+        WebElement username = driver.findElement(By.name("email"));
+        username.isDisplayed();
+        username.isEnabled();
+        username.sendKeys("admin@gmail.com");
+
+        // Provide password
+        WebElement password = driver.findElement(By.name("password"));
+        password.isDisplayed();
+        password.isEnabled();
+        password.sendKeys("ayesh12PAK");
+
+        // Clicking on a login button
+        WebElement loginButton = driver.findElement(By.name("submit"));
+        loginButton.isDisplayed();
+        loginButton.isEnabled();
+        loginButton.click();
+    }
 }
- 
